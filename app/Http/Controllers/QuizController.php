6e754bd9d3a0744ac3e1e2cb62aab64f4194b898
kumakku <s8.9.mv0e1mm2fd@gmail.com>
2023-08-19 17:53:56 +0900

    make_model_and_controller

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Quiz;
+
+class QuizController extends Controller
+{
+    //
+}
---
+++ app/Models/Quiz.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+    
+    //Userに対するリレーション
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    //Imageに対するリレーション
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+    
+    //Tagに対するリレーション
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
